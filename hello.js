9032605b3aae8799c2f66455b57f28318874d927
Himanisain99 <himanisain99@gmail.com>
2024-06-25 17:23:18 +0530

    quote string and number
    single and double qoute string
    integer and float

---
+++ hello.js
@@ -0,0 +1,10 @@
+
+
+let singleQoutestring='Himani';
+let doubleqoutestring="Sain";
+console.log(doubleqoutestring);
+console.log(singleQoutestring);
+let numbernotfoat= 45;
+let floatnotnumber= 2.3;
+console.log(numbernotfoat);
+console.log(floatnotnumber);

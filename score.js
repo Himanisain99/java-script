a2c8f63a57d81bf432989956f414e18527e1a55e
Himanisain99 <himanisain99@gmail.com>
2024-06-26 17:00:24 +0530

    conditional operator

--- score.js
+++ score.js
@@ -17,3 +17,20 @@ console.log(2*6)
 console.log(2**5)
 console.log(2/10)
 console.log(2%3)
+//this string concatination shows the two string jointment and combination
+let string1 = "Riya";
+let string2 = "Sharma";
+let outcome = `join two strings ${string1} and ${string2} is ${string1 + string2}.`;
+console.log(outcome);
+//in the below program we are using snake_upper_case pattern
+let MAX_VALUE = 90;
+let PT = 3.14;
+console.log(MAX_VALUE);
+
+
+console.log(2>1)
+console.log(2==1)
+console.log(2<1)
+console.log(2!=1)
+console.log(2>=1)
+
